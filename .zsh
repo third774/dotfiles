82091f2b5fe8a31e580fc89173c51611641ada38
Kevin Kipp <kevin.kipp@gmail.com>
2019-03-17 11:06:27 -0500

    Add history substring search

--- .zsh
+++ .zsh
@@ -13,3 +13,4 @@ source $ZSH/oh-my-zsh.sh
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 ZSH_AUTOSUGGEST_STRATEGY=match_prev_cmd
 source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/local/share/zsh-history-substring-search/zsh-history-substring-search.zsh

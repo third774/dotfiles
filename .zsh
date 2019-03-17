63909fac70719133cdabff4f3d7491166a4e7061
Kevin Kipp <kevin.kipp@gmail.com>
2019-03-17 10:48:31 -0500

    Add zsh-autosuggest

--- .zsh
+++ .zsh
@@ -10,3 +10,6 @@ plugins=(
 )
 
 source $ZSH/oh-my-zsh.sh
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+ZSH_AUTOSUGGEST_STRATEGY=match_prev_cmd
+source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh

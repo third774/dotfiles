7221ff0f5a52c4762f052e0f67441dfd7fe3254d
Kevin Kipp <kevin.kipp@gmail.com>
2023-10-22 20:44:52 -0500

    Add connect airpods script

---
+++ raycast-scripts/connect-to-airpods.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Connect to airpods
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author Kevin Kipp
+# @raycast.authorURL https://github.com/third774
+
+#!/bin/bash
+
+# Set the name of your AirPods Pro  
+airpods="Kevin’s AirPods Pro"
+
+SwitchAudioSource -a -t output
+
+# Get the full name of the AirPods Pro audio device
+airpods_device=$(SwitchAudioSource -a -t output | grep "$airpods" | sed 's/ (.*//')
+
+echo $airpods_device
+
+# Set AirPods Pro as the audio output device
+SwitchAudioSource -s "$airpods_device"

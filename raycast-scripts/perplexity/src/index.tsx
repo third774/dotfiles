777d9fd22bfdd1d783ab0a134dafd4f8cfda778b
Kevin Kipp <kevin.kipp@gmail.com>
2023-10-23 01:59:56 -0500

    Add Perplexity extension

---
+++ raycast-scripts/perplexity/src/index.tsx
@@ -0,0 +1,32 @@
+import { Form, ActionPanel, Action, showToast, open, popToRoot } from "@raycast/api";
+
+type Values = {
+  query: string;
+  copilot: boolean;
+};
+
+export default function Command() {
+  function handleSubmit({ query: q, copilot }: Values) {
+    const params = new URLSearchParams({
+      q,
+      copilot: copilot.toString(),
+    });
+    popToRoot();
+    open(`https://www.perplexity.ai/search?${params}`);
+  }
+
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm onSubmit={handleSubmit} />
+        </ActionPanel>
+      }
+    >
+      <Form.Description text="Where knowledge begins" />
+      <Form.TextArea id="query" title="Ask anything..." autoFocus />
+      <Form.Separator />
+      <Form.Checkbox id="copilot" title="Checkbox" label="Co-pilot" storeValue />
+    </Form>
+  );
+}

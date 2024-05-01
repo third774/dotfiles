00a39b1a2c332c7d0ebf1bcd430ab8102ac12480
Kevin Kipp <kevin.kipp@gmail.com>
2024-04-30 23:45:06 -0500

    Add zoxide

--- .zsh
+++ .zsh
@@ -16,4 +16,5 @@ ZSH_AUTOSUGGEST_STRATEGY=match_prev_cmd
 source /opt/homebrew/opt/zsh-autosuggestions/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /opt/homebrew/opt/zsh-history-substring-search/share/zsh-history-substring-search/zsh-history-substring-search.zsh
 
+eval "$(zoxide init zsh)"
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
